eef8686de0225f611e853f1f189ef59def3c0140
APIVerve Team <hello@apiverve.com>
2024-08-20 11:04:48 -0500

    Bug Fixes

--- RandomPunAPIClient.cs
+++ RandomPunAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, randompunQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, RandomPunQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(randompunQueryOptions options = null)
+        public ResponseObj Execute(RandomPunQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(randompunQueryOptions options)
+        private string constructURL(RandomPunQueryOptions options)
         {
             string url = _apiEndpoint;
 
